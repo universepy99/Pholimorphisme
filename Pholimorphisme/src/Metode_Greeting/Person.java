532b71358e0af499c513fe37d04cb2d49c01c492
universepy99 <114472231+universepy99@users.noreply.github.com>
2024-03-16 21:44:33 +0700

    Delete Pholimorphisme directory

--- Pholimorphisme/src/Metode_Greeting/Person.java
+++
@@ -1,8 +0,0 @@
-package Metode_Greeting;
-
-class Person {
-    // Metode untuk memberikan salam umum
-    public void greet() {
-        System.out.println("Halo, selamat datang!");
-    }
-}
--- Pholimorphisme/src/Metode_oprasiPenjualan/Product.java
+++
@@ -1,8 +0,0 @@
-package Metode_oprasiPenjualan;
-
-class Product {
-    // Metode untuk menampilkan informasi produk
-    public void info() {
-        System.out.println("Ini adalah produk.");
-    }
-}
